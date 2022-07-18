1a13268b2b0f64a49d811ce3f6a5d210bb64b218
Jyothi srinivasarao <sjyothi@altimetrik.com>
2022-07-18 12:03:25 +0530

    added config

--- conftest.py
+++ conftest.py
@@ -1,14 +1,18 @@
+import imp
 import pytest
 import allure
-from selenium import webdriver
+
+#from selenium import webdriver
 #from appium import webdriver
 import os
 import sys
 import pytest_bdd
 from datetime import datetime
 from typing import Union,List
+from pytest_html import extras
 import allure
 import ast
+from py.xml import html
 import requests
 import xlrd
 import pandas as pd
@@ -16,16 +20,18 @@ import json
 import numpy as np
 import base64
 import tweepy
+import config
+from pyallied.web.webWaits import customwebDriverwait
 
 def pytest_addoption(parser):
     parser.addoption(
-        "--b", action="store", default="type1", help="my option: type1 or type2"
+        "--b", action="store", default="chrme", help="my option: type1 or type2"
     )
     parser.addoption(
         "--e", action="store", default="qc", help="my option: type1 or type2"
     )
     parser.addoption(
-        "--t", action="store", default="qc", help="my option: type1 or type2"
+        "--t", action="store", default="web", help="my option: type1 or type2"
     )
 
 @pytest.fixture
@@ -42,27 +48,36 @@ def browser(b,t,request ):
     
     if(b=="chrome"):
         if(t=="web"):
+
+
+            from selenium import webdriver
+            from selenium.webdriver.common.by import By
+            from selenium.webdriver.remote.errorhandler import ErrorHandler
             opts = webdriver.ChromeOptions()
 
             opts.add_argument('--disable-gpu')
             
             browser = webdriver.Chrome("D:/Users/sjyothi/Downloads/chromedriver/chromedriver.exe", options=opts)
+            #browser2=webdriver.Firefox("D:/Users/sjyothi/Downloads/chromedriver/chromedriver.exe", options=opts)
             '''
             browser=webdriver.Remote(
                         command_executor='http://localhost:4444/wd/hub',
                         options=opts,
                         extensions=List[webdriver]
                     )
-            '''
-                    
-
-
-
+            
+                    '''
+           
+            customwebDriverwait.customWait=25
+            #print(customwebDriverwait.customWait," = custom wait")
+            print(browser  ,' ------->desired caps')
             yield browser
 
                 # Quit the WebDriver instance for the teardown
             browser.close()
-        else:
+        elif(t=="mob"):
+            from appium import webdriver
+            from appium.webdriver.common.touch_action import TouchAction
             
             desired_caps={
                             "platformName": "Android",
@@ -79,17 +94,52 @@ def browser(b,t,request ):
             #driver = webdriver.Chrome("D:/Users/sjyothi/Downloads/chromedriver/chromedriver.exe", options=opts)
             browser=webdriver.Remote(
                         'http://localhost:4723/wd/hub',
-                        desired_caps
+                        desired_caps,
                     )
 
 
             #browser.set_page_load_timeout(5000)
             #browser.set_script_timeout(5000)
-            #browser.implicitly_wait(5)
+            #browser.switch_to.parent_frame()
             yield browser
 
                 # Quit the WebDriver instance for the teardown
-            browser.quit() 
+            browser.quit()
+        else:
+            print("*******noDriver Selected*********")     
+
+    elif(b=="firefox"):
+        if(t=="web"):
+
+
+            from selenium import webdriver
+            from selenium.webdriver.common.by import By
+            from selenium.webdriver.remote.errorhandler import ErrorHandler
+            opts = webdriver.ChromeOptions()
+
+            opts.add_argument('--disable-gpu')
+            
+            browser = webdriver.Firefox(executable_path=r"D:/Users/sjyothi/Downloads/geckodriver/geckodriver.exe")
+            #browser2=webdriver.Firefox("D:/Users/sjyothi/Downloads/chromedriver/chromedriver.exe", options=opts)
+            '''
+            browser=webdriver.Remote(
+                        command_executor='http://localhost:4444/wd/hub',
+                        options=opts,
+                        extensions=List[webdriver]
+                    )
+            
+                    '''
+           
+            customwebDriverwait.customWait=25
+            #print(customwebDriverwait.customWait," = custom wait")
+            print(browser  ,' ------->desired caps')
+            yield browser
+
+                # Quit the WebDriver instance for the teardown
+            browser.close()
+        
+
+
 '''
 @pytest.fixture
 def crud_call():
@@ -156,7 +206,7 @@ def pytest_pyfunc_call(pyfuncitem):
             print(pyfuncitem.funcargs['watch_logs']())
 
 
-
+'''
 
 # for html report with screenshots, 'do notmodify it'
 @pytest.hookimpl(hookwrapper=True)
@@ -167,19 +217,49 @@ def pytest_runtest_makereport(item,call):
     report = outcome.get_result()
     #setattr()
     extra = getattr(report, "extra", [])
+    #extra.append(extras.html("<div>Additional HTML</div>"))  
+    #extra.append(extras.image(config.rootPath()+"/screen1.png"))
     if report.when == "call":
+        #print(item.runtest(),'------------user')
+        #extra.append(extras.html("<div>Additionals HTML</div>"))  
         feature_request = item.funcargs['request']
-        #print(feature_request,"report")
+        print(item.funcargs['request'],"-----report-------")
         driver = feature_request.getfixturevalue('browser')
         nodeid = item.nodeid
         xfail = hasattr(report, "wasxfail")
         if (report.skipped and xfail) or (report.failed and not xfail):
             file_name = f'{nodeid}_{datetime.today().strftime("%Y-%m-%d_%H_%M")}.png'.replace("/", "_").replace("::", "_").replace(".py", "")
-            img_path = os.path.join("D:\\Users\\sjyothi\\Repos\\pythonseleniumFramework\\features", "screenshots", file_name)
-            driver.save_screenshot(img_path)
+            #img_path = os.path.join("D:\\Users\\sjyothi\\Repos\\pythonseleniumFramework\\features", "screenshots", file_name)
+            #driver.save_screenshot(img_path)
             screenshot = driver.get_screenshot_as_base64() # the hero
             extra.append(pytest_html.extras.image(screenshot, ''))
         report.extra = extra
+@pytest.fixture(autouse=True)
+def update_env_file(b,t,e):
+    with open(config.rootPath()+"/env.json", "r+") as jsonFile:
+        data = json.load(jsonFile)
 
-       
-''' 
+        data["env"] = e
+        data["browser_name"] = b
+        data["test_platform"] = t
+        
+
+        jsonFile.seek(0)  # rewind
+        json.dump(data, jsonFile)
+        jsonFile.truncate()
+
+@pytest.hookimpl(tryfirst=True)
+def pytest_sessionfinish(session, exitstatus):
+      with open(config.rootPath()+"/env.json", "r+") as jsonFile:
+        data = json.load(jsonFile)
+        session.config._metadata["browserName"] =data["browser_name"]
+        session.config._metadata["environment"]=data["env"]
+        session.config._metadata["test_platform"]=data["test_platform"]
+        jsonFile.close()
+@pytest.hookimpl()
+def pytest_html_report_title(report):
+    report.title = "My very own title!"
+@pytest.hookimpl()    
+def test_extra(extra):
+    extra.append(extras.html("<div>Additionalsss HTML</div>"))    
+  
